c89ff07b4ce0367c9629f2d26d3bc5ebec85e0f4
BozoB80 <bozo.bakula@gmail.com>
2024-01-14 18:10:07 +0100

    fix

--- components/navbar/Searchbar.tsx
+++ components/navbar/Searchbar.tsx
@@ -2,7 +2,7 @@
 
 import { Search, X } from "lucide-react";
 import { Input } from "../ui/input";
-import { ChangeEventHandler, KeyboardEventHandler, useState } from "react";
+import { ChangeEventHandler, KeyboardEventHandler, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Dialog, DialogClose, DialogContent, DialogTrigger } from "../ui/dialog";
 import { Button } from "../ui/button";
@@ -10,20 +10,32 @@ import { Button } from "../ui/button";
 const Searchbar = () => {
   const [value, setValue] = useState('')
   const router = useRouter()
+  const dialogCloseRef = useRef<React.ElementRef<typeof DialogClose>>(null);
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setValue(e.target.value)    
   }
 
   const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (!value) return
     if (e.key === 'Enter') {
       router.push(`/pretraga?q=${encodeURIComponent(value)}`)
+      closeDialog()
     }
   };
 
   const onClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    if (!value) return
     router.push(`/pretraga?q=${encodeURIComponent(value)}`)
   }
+
+  const closeDialog = () => {
+    // Check if the ref is defined before attempting to close
+    if (dialogCloseRef.current) {
+      // Trigger the close action
+      dialogCloseRef.current.click();
+    }
+  };
 
   return (
     <>
@@ -50,7 +62,7 @@ const Searchbar = () => {
               <Search size={24} />  
             </Button>
           </DialogTrigger>
-          <DialogContent className="max-w-sm">
+          <DialogContent className="max-w-sm top-40">
             <div className="relative">
               <Search
                 className="h-4 w-4 absolute top-3 left-1"
@@ -66,7 +78,7 @@ const Searchbar = () => {
                 <X onClick={() => setValue("")} className="h-4 w-4 absolute top-3 right-1 cursor-pointer" />
               )}
             </div>
-            <DialogClose asChild>
+            <DialogClose ref={dialogCloseRef} asChild>
               <Button onClick={onClick}>
                 Pretraži
               </Button>
